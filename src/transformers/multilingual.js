3e5a76574134a7a4787c99f1750c757e42f88bd4
Gertt <Gertt@users.noreply.github.com>
2017-10-04 13:38:01 +0200

    [multilingual] add languageCase option

--- src/transformers/multilingual.js
+++ src/transformers/multilingual.js
@@ -1,30 +1,62 @@
+const SNAKE_CASE = 'SNAKE_CASE';
+const CAMEL_CASE = 'CAMEL_CASE';
+
 function ucfirst(str) {
     return str.replace(/^[a-z]/, l => l.toUpperCase());
 }
 
-function compileRegex(languages) {
+function createLanguageSuffix(language, casing) {
+    if (casing === CAMEL_CASE)
+        language = ucfirst(language);
+    else if (casing === SNAKE_CASE)
+        language = '_' + language;
+
+    return language;
+}
+
+function restoreLanguageSuffix(suffix, casing) {
+    if (casing === CAMEL_CASE)
+        suffix = suffix.toLowerCase();
+    else if (casing === SNAKE_CASE)
+        suffix = suffix.replace(/^_/, '');
+
+    return suffix;
+}
+
+function compileRegex(languages, suffixer) {
     const langSuffix = languages
-        .map(l => ucfirst(l))
+        .map(l => suffixer(l))
         .join('|');
 
     return new RegExp(`(.+)(${langSuffix})$`);
 }
 
-function toLowerCase(key /* , value */) {
-    return key.toLowerCase();
+function createRestorer(casing) {
+    return function(suffix) {
+        return restoreLanguageSuffix(suffix, casing);
+    };
 }
 
-function toPascalCase(key /* , value */) {
-    return ucfirst(key);
+function createSuffixer(casing) {
+    return function(language) {
+        return createLanguageSuffix(language, casing);
+    };
 }
 
-function MultilingualTransformer(languages) {
+function MultilingualTransformer(languages, languageCase) {
     languages = languages || this.getOption('multilingual.languages') || [];
-    const regex = compileRegex(languages);
+    languageCase = languageCase || this.getOption('multilingual.languageCase') || CAMEL_CASE;
+
+    const suffixer = createSuffixer(languageCase);
+    const restorer = createRestorer(languageCase);
+    const regex = compileRegex(languages, suffixer);
 
     return this.match(regex)
         .group(regex, 1, 2)
-        .map(p => p.rename(toLowerCase, toPascalCase));
+        .map(p => p.rename(restorer, suffixer));
 }
+
+MultilingualTransformer.SNAKE_CASE = SNAKE_CASE;
+MultilingualTransformer.CAMEL_CASE = CAMEL_CASE;
 
 module.exports = MultilingualTransformer;
--- test/transformers/multilingual.js
+++ test/transformers/multilingual.js
@@ -24,7 +24,7 @@ describe('multilingual', function() {
                 multilingual: Multilingual,
                 match: match
             };
-            const instance = obj.multilingual(['en', 'nl']);
+            const instance = obj.multilingual(['en', 'nl'], Multilingual.CAMEL_CASE);
 
             assert.equal(instance, multilingual);
         })
@@ -44,7 +44,10 @@ describe('multilingual', function() {
             const obj = {
                 multilingual: Multilingual,
                 match: match,
-                getOption: function() { return ['en', 'nl', 'fr']; }
+                getOption: function(key) {
+                    if (key == 'multilingual.languages')
+                        return ['en', 'nl', 'fr'];
+                }
             };
             const instance = obj.multilingual();
 
@@ -125,6 +128,40 @@ describe('multilingual', function() {
                 getOption: function() { }
             };
             const instance = obj.multilingual();
+
+            assert.equal(instance, multilingual);
+        })
+
+        it('Should support snake case', function () {
+            const multilingual = function() {};
+            const renamer = function(restorer, suffixer) {
+                assert.equal(typeof restorer, 'function');
+                assert.equal(typeof suffixer, 'function');
+
+                assert.equal(restorer('_abc'), 'abc');
+                assert.equal(suffixer('abc'), '_abc');
+            }
+            const map = function(f) {
+                assert.equal(typeof f, 'function');
+                f({ rename: renamer });
+
+                return multilingual;
+            };
+            const group = function(regex, m1, m2) {
+                assert.equal(regex.toString(), '/(.+)(_en|_fr)$/');
+                return { map: map };
+            };
+            const match = function(regex) {
+                assert.equal(regex.toString(), '/(.+)(_en|_fr)$/');
+                return { group: group };
+            };
+
+            const obj = {
+                multilingual: Multilingual,
+                match: match,
+                getOption: function() { }
+            };
+            const instance = obj.multilingual([ 'en', 'fr' ], Multilingual.SNAKE_CASE);
 
             assert.equal(instance, multilingual);
         })
